d906e00119c4229a804067f098cf91c7a52c2be4
kud202 <karandesai@nyu.edu>
2016-05-03 18:00:07 -0400

    Update query.lua

--- query.lua
+++ query.lua
@@ -325,6 +325,7 @@ function run_query()
           io.write(line[i])
           io.write(' ')
         end
+        state_query = torch.Tensor(state_query)
         reset_state(state_query)
         g_disable_dropout(model.rnns)
         g_replace_table(model.s[0], model.start_s)
