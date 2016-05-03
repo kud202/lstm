6604b4ac2ba7d73411630ef2f5f524097c9eb670
kud202 <karandesai@nyu.edu>
2016-05-03 14:00:53 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -22,12 +22,12 @@ ptb = require('data')
 
 -- Trains 1 epoch and gives validation set ~182 perplexity (CPU).
 local params = {
-                batch_size=20, -- minibatch
-                seq_length=20, -- unroll length
+                batch_size=10, -- minibatch
+                seq_length=15, -- unroll length
                 layers=2,
                 decay=2,
                 rnn_size=200, -- hidden unit size
-                dropout=0, 
+                dropout=0.2, 
                 init_weight=0.1, -- random weight initialization limits
                 lr=1, --learning rate
                 vocab_size=10000, -- limit on the vocabulary size
@@ -46,30 +46,70 @@ end
 
 model = {}
 
-local function lstm(x, prev_c, prev_h)
-    -- Calculate all four gates in one go
-    local i2h              = nn.Linear(params.rnn_size, 4*params.rnn_size)(x)
-    local h2h              = nn.Linear(params.rnn_size, 4*params.rnn_size)(prev_h)
+--local function lstm(x, prev_c, prev_h)
+  local function lstm(x, prev_h)
+    --[[-- Calculate all four gates in one go
+    --local i2h              = nn.Linear(params.rnn_size, 4*params.rnn_size)(x)
+    local i2h              = nn.Linear(params.rnn_size, 2*params.rnn_size)(x)
+    --local h2h              = nn.Linear(params.rnn_size, 4*params.rnn_size)(prev_h)
+    local h2h              = nn.Linear(params.rnn_size, 2*params.rnn_size)(prev_h)
     local gates            = nn.CAddTable()({i2h, h2h})
-
     -- Reshape to (batch_size, n_gates, hid_size)
     -- Then slize the n_gates dimension, i.e dimension 2
-    local reshaped_gates   =  nn.Reshape(4,params.rnn_size)(gates)
+    --local reshaped_gates   =  nn.Reshape(4,params.rnn_size)(gates)
+    local reshaped_gates   =  nn.Reshape(2,params.rnn_size)(gates)
     local sliced_gates     = nn.SplitTable(2)(reshaped_gates)
-
     -- Use select gate to fetch each gate and apply nonlinearity
     local in_gate          = nn.Sigmoid()(nn.SelectTable(1)(sliced_gates))
-    local in_transform     = nn.Tanh()(nn.SelectTable(2)(sliced_gates))
-    local forget_gate      = nn.Sigmoid()(nn.SelectTable(3)(sliced_gates))
-    local out_gate         = nn.Sigmoid()(nn.SelectTable(4)(sliced_gates))
+    --local in_transform     = nn.Tanh()(nn.SelectTable(2)(sliced_gates))
+    local forget_gate      = nn.Sigmoid()(nn.SelectTable(2)(sliced_gates))
+    --local out_gate         = nn.Sigmoid()(nn.SelectTable(4)(sliced_gates))
+    
+    local new_h = nn.CMulTable()({forget_gate,prev_h})
+    
+    local i2newh    =nn.Linear(params.rnn_size , params.rnn_size)(x)
+    local newh2newh =nn.Linear(params.rnn_size , params.rnn_size)(new_h)
+    
+    local transform_gate    =nn.CAddTable()({i2newh , newh2newh})
+    
+    local reshape_gate      =nn.Reshape(1,params.rnn_size)(transform_gate)
+    local slice_gate        =nn.SplitTable(1)(reshape_gate)
+    local in_transform      =nn.Tanh()(nn.SelectTable(1)(slice_gate))    
+    
+    --local next_c           = nn.CAddTable()({
+    --    nn.CMulTable()({forget_gate, prev_c}),
+    --    nn.CMulTable()({in_gate,     in_transform})
+    --})
+    --local next_h           = nn.CMulTable()({out_gate, nn.Tanh()(next_c)})
+    
+    --local res=torch.Tensor():resizeAs(in_gate):fill(1)
+    local res= nn.Copy(in_gate):fill(1)
+    local newin_gate    = nn.CSubTable()({res,in_gate*-1})         
+    local next_h        = nn.CAddTable()({
+        nn.CMulTable()({prev_h,newin_gate}),
+        nn.CMulTable()({in_transform, newin_gate})        
+        })--]]
+    
+    local i2h   =nn.Linear(params.rnn_size, 3 *params.rnn_size)(x)
+    
+    local h2h   =nn.Linear(params.rnn_size, 3 * params.rnn_size)(prev_h) 
 
-    local next_c           = nn.CAddTable()({
-        nn.CMulTable()({forget_gate, prev_c}),
-        nn.CMulTable()({in_gate,     in_transform})
-    })
-    local next_h           = nn.CMulTable()({out_gate, nn.Tanh()(next_c)})
+    local gates =nn.CAddTable()({nn.Narrow(2, 1, 2 *params.rnn_size)(i2h),
+                                nn.Narrow(2, 1, 2 *params.rnn_size)(h2h)
+                                })
+                            
+    gates =nn.SplitTable(2)(nn.Reshape(2,params.rnn_size)(gates))
+    
+    local resetgate  =nn.Sigmoid()(nn.SelectTable(1)(gates)) 
 
-    return next_c, next_h
+    local updategate =nn.Sigmoid()(nn.SelectTable(2)(gates)) 
+    
+    local output =nn.Tanh()(nn.CAddTable()({ nn.Narrow(2, 2 * params.rnn_size+1, params.rnn_size)(i2h),
+                                            nn.CMulTable()({resetgate, nn.Narrow(2, 2 * params.rnn_size+1, params.rnn_size)(h2h)}) })) 
+                                        
+    local next_h = nn.CAddTable()({ prev_h,nn.CMulTable()({ updategate, nn.CSubTable()({output, prev_h,}),}), }) 
+
+    return next_h
 end
 
 function create_network()
@@ -79,22 +119,26 @@ function create_network()
     local i                  = {[0] = nn.LookupTable(params.vocab_size,
                                                     params.rnn_size)(x)}
     local next_s             = {}
-    local split              = {prev_s:split(2 * params.layers)}
+    --local split              = {prev_s:split(2 * params.layers)}
+    local split              = {prev_s:split(params.layers)}
     for layer_idx = 1, params.layers do
-        local prev_c         = split[2 * layer_idx - 1]
-        local prev_h         = split[2 * layer_idx]
+        --local prev_c         = split[2 * layer_idx - 1]
+        --local prev_h         = split[2 * layer_idx]
+        local prev_h         = split[layer_idx]
         local dropped        = nn.Dropout(params.dropout)(i[layer_idx - 1])
-        local next_c, next_h = lstm(dropped, prev_c, prev_h)
-        table.insert(next_s, next_c)
+        --local next_c, next_h = lstm(dropped, prev_c, prev_h)
+        local next_h = lstm(dropped, prev_h)
+        --table.insert(next_s, next_c)
         table.insert(next_s, next_h)
         i[layer_idx] = next_h
     end
+    
     local h2y                = nn.Linear(params.rnn_size, params.vocab_size)
     local dropped            = nn.Dropout(params.dropout)(i[params.layers])
     local pred               = nn.LogSoftMax()(h2y(dropped))
     local err                = nn.ClassNLLCriterion()({pred, y})
     local module             = nn.gModule({x, y, prev_s},
-                                      {err, nn.Identity()(next_s)})
+                                      {err, nn.Identity()(next_s),pred})
     -- initialize weights
     module:getParameters():uniform(-params.init_weight, params.init_weight)
     return transfer_data(module)
@@ -105,28 +149,34 @@ function setup()
     local core_network = create_network()
     paramx, paramdx = core_network:getParameters()
     model.s = {}
+    --model.pred={}
     model.ds = {}
     model.start_s = {}
     for j = 0, params.seq_length do
         model.s[j] = {}
-        for d = 1, 2 * params.layers do
+        --for d = 1, 2 * params.layers do
+        for d = 1, params.layers do
             model.s[j][d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
+            --model.pred[j][d]=transfer_data(torch.zeros(params.batch_size, params.rnn_size))
         end
     end
-    for d = 1, 2 * params.layers do
+    --for d = 1, 2 * params.layers do
+    for d = 1, params.layers do
         model.start_s[d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
         model.ds[d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
     end
     model.core_network = core_network
     model.rnns = g_cloneManyTimes(core_network, params.seq_length)
     model.norm_dw = 0
+    model.pred= transfer_data(torch.zeros(params.seq_length))
     model.err = transfer_data(torch.zeros(params.seq_length))
 end
 
 function reset_state(state)
     state.pos = 1
     if model ~= nil and model.start_s ~= nil then
-        for d = 1, 2 * params.layers do
+        --for d = 1, 2 * params.layers do
+        for d = 1, params.layers do
             model.start_s[d]:zero()
         end
     end
@@ -150,11 +200,16 @@ function fp(state)
     -- forward prop
     for i = 1, params.seq_length do
         local x = state.data[state.pos]
+        --print(x:size())
         local y = state.data[state.pos + 1]
+        --print(y:size())
         local s = model.s[i - 1]
-        model.err[i], model.s[i] = unpack(model.rnns[i]:forward({x, y, s}))
+        --print(model.rnns[i]:forward({x, y, s}):size())
+        model.err[i], model.s[i],pred = unpack(model.rnns[i]:forward({x, y, s}))
         state.pos = state.pos + 1
     end
+    
+    --print(model.pred)
     
     -- next-forward-prop start state is current-forward-prop's last state
     g_replace_table(model.start_s, model.s[params.seq_length])
@@ -167,6 +222,7 @@ function bp(state)
     -- start on a clean slate. Backprop over time for params.seq_length.
     paramdx:zero()
     reset_ds()
+    --local pred = torch.zeros(params.seq_length)
     for i = params.seq_length, 1, -1 do
         -- to make the following code look almost like fp
         state.pos = state.pos - 1
@@ -174,10 +230,12 @@ function bp(state)
         local y = state.data[state.pos + 1]
         local s = model.s[i - 1]
         -- Why 1?
+        
         local derr = transfer_data(torch.ones(1))
+        local dpred = transfer_data(torch.zeros(params.batch_size, params.vocab_size))
         -- tmp stores the ds
         local tmp = model.rnns[i]:backward({x, y, s},
-                                           {derr, model.ds})[3]
+                                           {derr, model.ds,dpred})[3]
         -- remember (to, from)
         g_replace_table(model.ds, tmp)
     end
@@ -258,6 +316,8 @@ print("Starting training.")
 words_per_step = params.seq_length * params.batch_size
 epoch_size = torch.floor(state_train.data:size(1) / params.seq_length)
 
+--print(state_train.data:size())
+
 while epoch < params.max_max_epoch do
 
     -- take one step forward
@@ -285,6 +345,10 @@ while epoch < params.max_max_epoch do
              ', dw:norm() = ' .. g_f3(model.norm_dw) ..
              ', lr = ' ..  g_f3(params.lr) ..
              ', since beginning = ' .. since_beginning .. ' mins.')
+         
+        print('saving core')
+        torch.save('core.net',model.core_network)
+        print('saved core')
     end
     
     -- run when epoch done
