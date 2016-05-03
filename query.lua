6326cd67963cb715b12b38ea47ace11a8ac0f683
kud202 <karandesai@nyu.edu>
2016-05-03 17:29:44 -0400

    Update query.lua

--- query.lua
+++ query.lua
@@ -148,7 +148,7 @@ end
 
 function setup()
     print("Creating a RNN LSTM network.")
-    local core_network = torch.load('core.net')
+    local core_network = create_network()
     paramx, paramdx = core_network:getParameters()
     model.s = {}
     --model.pred={}
